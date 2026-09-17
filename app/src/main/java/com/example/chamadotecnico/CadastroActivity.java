ff34941472d14f0d9407a85cc4acdfc28aacd5dc
YuuY314 <rafaelyu0202@gmail.com>
2026-09-17 10:50:06 -0300

    Aula 17/09/2026

---
+++ app/src/main/java/com/example/chamadotecnico/CadastroActivity.java
@@ -0,0 +1,72 @@
+package com.example.chamadotecnico;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private Button btnCadastrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cadastro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbarTop);
+        setSupportActionBar(toolbar);
+
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Intent intent = new Intent(CadastroActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(CadastroActivity.this, "Cadastrado com sucesso!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_top, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(item.getItemId() == R.id.menu_config){
+            Toast.makeText(this, "Configurações Selecionadas", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if(item.getItemId() == R.id.menu_sobre){
+            Intent intent = new Intent(CadastroActivity.this, SobreActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/chamadotecnico/MainActivity.java
+++ app/src/main/java/com/example/chamadotecnico/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.chamadotecnico;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -30,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbarTop);
+        setSupportActionBar(toolbar);
 
         ilEquipamento = findViewById(R.id.ilEquipamento);
         ilPatrimonio = findViewById(R.id.ilPatrimonio);
@@ -73,5 +81,33 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Chamado aberto com sucesso!", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_top, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(item.getItemId() == R.id.menu_config){
+            Toast.makeText(this, "Configurações Selecionadas", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if(item.getItemId() == R.id.menu_sobre){
+            Intent intent = new Intent(MainActivity.this, SobreActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if(item.getItemId() == R.id.menu_cadastro){
+            Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
